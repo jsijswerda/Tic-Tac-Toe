1bd407cf9dc93e0e2f3647df47f4144b014b84b6
Jasper <jasper.syswerda@gmail.com>
2020-03-17 00:09:44 +0100

    added dummy player test

---
+++ src/test/java/com/jasper/PlayerTest.java
@@ -0,0 +1,13 @@
+package com.jasper;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void PlayerTest() {
+        assertTrue(true);
+    }
+}
