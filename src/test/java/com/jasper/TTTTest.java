67578354784ed8615b601bfc7ad22f6cdb1b7ade
Jasper <jasper.syswerda@gmail.com>
2020-03-17 00:05:46 +0100

    Maven project made

---
+++ src/test/java/com/jasper/TTTTest.java
@@ -0,0 +1,20 @@
+package com.jasper;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TTTTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
